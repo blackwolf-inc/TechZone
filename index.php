313e22ba4dc47647f0f38d461a3a116006aa79dc
fatepsycho <fatepsycho@gmail.com>
2020-04-10 15:35:35 +0700

    Update index.php
    - fix responsive

--- index.php
+++ index.php
@@ -133,6 +133,12 @@
       div#boxvideoscontent > div.col-12 > h7 > a:hover {
         color: #cc0099;
       }
+      
+      div.col-lg-4 > div#titlekolfeaturedvideo > h6#titlefeaturedvideo {
+        color: #ff3399; 
+        border-bottom: solid 2px #6600ff; 
+        border-left: solid 2px #6600ff;
+      }
 
       h6#titlemorevideous > a {
         color: #ff3399;
@@ -158,6 +164,10 @@
 
 
       @media (max-width: 575px) {
+
+        div.col-lg-4 > div#titlekolfeaturedvideo > h6#titlefeaturedvideo {
+        border: none;
+        }
 
         .jumbotron {
         background-color: white;
@@ -226,6 +236,17 @@
         .jumbotron {
         background-color: white;
         background-image: none;
+        }
+
+        div.col-lg-4 > div#titlekolfeaturedvideo > h6#titlefeaturedvideo {
+        border: none;
+        }
+
+      }
+
+      @media (max-width: 992px) {
+        div.col-lg-4 > div#titlekolfeaturedvideo > h6#titlefeaturedvideo {
+        border: none;
         }
 
       }
@@ -421,8 +442,8 @@
   </div>
 
     <div class="col-lg-4 col-md-12 mt-3 pl-lg-5 mb-0">
-    <div class="col pl-lg-0 pt-0" style="color: #ff3399;">
-    <h6 class="text-left font-weight-bold pr-5 pl-1 pb-2 mr-3 ml-3 mt-0 pt-0 pl-3 d-inline-block mr-3 mb-0" style="color: #ff3399; border-bottom: solid 2px #6600ff; border-left: solid 2px #6600ff;">FEATURED VIDEO</h6>
+    <div class="col pl-lg-0 pl-0 pt-0 d-flex justify-content-lg-start justify-content-center" style="color: #ff3399;" id="titlekolfeaturedvideo">
+    <h6 class="font-weight-bold pr-5 pl-1 pb-2 mr-3 ml-3 mt-0 pt-0 pl-3 d-inline-block mr-3 mb-0" id="titlefeaturedvideo">FEATURED VIDEO</h6>
     </div>
     <div class="row mr-3 ml-0 pt-3 mt-0" id="boxvideoscontent">
 
@@ -456,11 +477,11 @@
 // end while
  ?>
 <!-- loping vidio end  -->
+  </div>
+    <h6 class="pr-2 font-weight-bold pt-2 pb-2 pl-2 ml-3 mr-4 float-right" id="titlemorevideous" style="color: #ff3399; border-bottom: 1px solid #6600ff; border-right: 1px solid #6600ff;"><a href="">MORE VIDEO</a></h6>
+  </div>
 
 
-  </div>
-      <h6 class="text-right pr-lg-2 font-weight-bold pt-lg-2 pb-lg-2 pl-lg-2 ml-lg-3 mr-lg-3 float-right pull-right" id="titlemorevideous" style="color: #ff3399; border-bottom: 1px solid #6600ff; border-right: 1px solid #6600ff;"><a href="">MORE VIDEO</a></h6>
-  </div>
 
   <!-- More Stories -->
     <div class="col pt-sm-3 pl-sm-0 pr-sm-0 pl-lg-3 pr-lg-3">
@@ -515,9 +536,11 @@ $(window).resize(function() {
 if ($(this).width() < 575) {
     $(".navbar-brand").show();
     $("#boxvideoscontent").css("border","none");
-
     $("#kolomcarousel1").css("margin-bottom","0");
     $("#kolomcarousel2").css("margin-bottom","0");
+
+    $("#titlekolfeaturedvideo").css("padding-left","0");
+    
     
 } else if ($(this).width() < 767) {
     $(".navbar-brand").show();
