3e55ae053e5daec6a712978e471ebe67103c7142
bagas <INC.blackwolf@gmail.com>
2020-04-10 11:36:03 +0700

    tambahan halamn utama

--- future.php
+++ future.php
@@ -220,14 +220,6 @@
 <body>
 <!-- JumbNavbar -->
 <div class="pos-f-t">
-  <div id="navbarToggleExternalContent">
-    <div class="bg p-4 pt-5 pb-5">
-      <div class="row" style="display: block;">
-        <div class="col"><h4 class="text-center text-white"><i>TECHZONE</i></h4></div>
-        <div class="col"><h7 class="text-center text-white" style="display: block;">Friday, March 27, 2020 | THE MAC FOR MOST</h7></div>
-      </div>
-    </div>
-  </div>
   <!-- Nav -->
   <nav class="navbar navbar-expand-lg navbar-dark">
   <a class="navbar-brand font-weight-bold" href="#" style="display: none;"><i>TECHZONE</i></a>
@@ -235,15 +227,16 @@
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
+ <img src="admin/aset_gambar/zonetech.PNG">
     <ul class="navbar-nav nav-justified w-50 m-auto">
-      <li class="nav-item ">
-        <a class="nav-link active" href="index.php">HOME <span class="sr-only">(current)</span></a>
+       <li class="nav-item ">
+        <a class="nav-link " href="index.php">HOME <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item ">
-        <a class="nav-link" href="new_news.php">NEW NEWS <span class="sr-only">(current)</span></a>
+        <a class="nav-link " href="new_news.php">NEW NEWS <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="future.php">FUTURE</a>
+      <li class="nav-item active">
+        <a class="nav-link active" href="future.php">FUTURE</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">SEE ALL NEWS</a>
@@ -260,111 +253,29 @@
   <div class="row mt-3 carouselbox">
     <!-- kolom 1 -->
     <div class="col-lg-8 col-md-8 col-sm-12" style="border: solid 2px; border-image:  linear-gradient(-90deg, #ff3399, #ff9900) 1;">
-      <div class="row">
-        <div class="col p-0" id="kolomcarousel1">
-          <img src="assets/img/TopPost/1.jpg" alt="" class="img-fluid h-100">
-        </div>
-        <div class="col mt-2 p-lg-4 pl-lg-4">
-          <h1 class="text-white font-weight-bold text-uppercase"><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, earum!</a><br><h5 class="text-white" style="display: inline-block;">BY <a href="" style="color: #ff3399;">Lorem ipsum dolor.</a></h5></h1>
-          <hr style="border: 1px solid #ff3399; width: 50%; position: absolute; bottom: auto; right: 0; margin-top: 0px;">
-        </div>
-      </div>
-    </div>
+  
     <!-- end kolom 1 -->
 
     <!-- kolom 2 -->
-    <div class="col-lg-4 col-md-4 col-sm-6" style="border: solid 2px; border-image:  linear-gradient(-90deg, #ff3399, #ff9900) 1;">
-      <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-0">
-          <img src="assets/img/TopPost/2.jpg" alt="" class="img-fluid">
-        </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-lg-4 pl-lg-4">
-          <h4 class="font-weight-bold text-lowercase"><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing.</a><br><h5 class="text-white">BY <a href="" style="color: #ff3399;">ANONIM</a></h5></h4>
-        </div>
-      </div>
-    </div>
+    
     <!-- end kolom 2 -->
 
     <!-- kolom 3 -->
-    <div class="col-md-4 col-sm-6 col-lg-4" style="border: solid 2px; border-image:  linear-gradient(-90deg, #ff3399, #ff9900) 1;">
-      <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-0">
-          <img src="assets/img/TopPost/3.jpg" alt="" class="img-fluid">
-        </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-lg-4 pl-lg-4">
-          <h4 class="text-white font-weight-bold text-lowercase"><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing.</a><br><h5 class="text-white">BY <a href="" style="color: #ff3399;">ANONIM</a></h5></h4>
-        </div>
-      </div>
-    </div>
+    
     <!-- end kolom 3 -->
 
     <!-- kolom 4 -->
-    <div class="col-md-8 col-lg-8" style="border: solid 2px; border-image:  linear-gradient(-90deg, #ff3399, #ff9900) 1;">
-      <div class="row">
-        <div class="col p-0" id="kolomcarousel2">
-          <img src="assets/img/TopPost/3.jpg" alt="" class="img-fluid h-100">
-        </div>
-        <div class="col p-lg-4 pl-lg-4">
-          <h1 class="text-white font-weight-bold text-uppercase"><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim, earum!</a><br><h5 class="text-white">BY <a href="" style="color: #ff3399;">ANONIM</a></h5></h1>
-          <hr style="border: 1px solid #ff3399; width: 50%; position: absolute; bottom: auto; right: 0; margin-top: 0px;">
-        </div>
-      </div>
-    </div>
+    
     <!-- end kolom 4 -->
   </div>
-  <div class="row mt-0 carouselbox">
-    <!-- kolom 5 -->
-    <div class="col-lg-4 col-md-4 col-sm-4" style="border: solid 2px; border-image:  linear-gradient(-90deg, #ff3399, #ff9900) 1;">
-      <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-0">
-          <img src="assets/img/TopPost/5.jpg" alt="" class="img-fluid" style="width: 100%; max-height: 400px;">
-        </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-lg-4 pl-lg-4">
-           <h4 class="text-white font-weight-bold text-lowercase"><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing.</a><br><h5 class="text-white">BY <a href="" style="color: #ff3399;">ANONIM</a></h5></h4>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-md-4 col-sm-4" style="border: solid 2px; border-image:  linear-gradient(-90deg, #ff3399, #ff9900) 1;">
-      <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-0">
-          <img src="assets/img/TopPost/6.jpg" alt="" class="img-fluid" style="width: 100%; max-height: 400px;">
-        </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-lg-4 pl-lg-4">
-           <h4 class="text-white font-weight-bold text-lowercase"><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing.</a><br><h5 class="text-white">BY <a href="" style="color: #ff3399;">ANONIM</a></h5></h4>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4 col-md-4 col-sm-4" style="border: solid 2px; border-image:  linear-gradient(-90deg, #ff3399, #ff9900) 1;">
-      <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-0">
-          <img src="assets/img/TopPost/7.jpg" alt="" class="img-fluid w-100" style="width: 100%; max-height: 400px;">
-        </div>
-        <div class="col-lg-12 col-md-12 col-sm-12 col p-lg-4 pl-lg-4">
-           <h4 class="text-white font-weight-bold text-lowercase"><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing.</a><br><h5 class="text-white">BY <a href="" style="color: #ff3399;">ANONIM</a></h5></h4>
-        </div>
-      </div>
-    </div>
+  
+    
     <!-- end kolom 5 -->
   </div>
 <!-- end Carousel 1 -->
 
 <!-- Jumbotron  -->
-<div class="row">
-      <div class="col-lg-12 col-md-12 col-sm-12 pl-0 pr-0">
-          <div class="jumbotron jumbotron-fluid mb-0">
-            <div class="container">
-              <div class="row">
-                <div class="col-6 mt-xs-5 mt-sm-5 mt-md-5 mt-lg-5 mt-4 pr-0 pr-5">
-                  <h5 class="font-weight-bold text-right"><a href="">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, esse!</a><br><h7 class="text-white">BY <a href="" style="color: #ff3399;">ANONIM</a></h7></h5>
-                </div>
-                <div class="col-6 pl-1">
-                  <h1 class="text-uppercase font-weight-bold font-italic"><a href="">"Lorem ipsum dolor sit amet, consectetur adipisicing elit"</a></h1>
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
-    </div>
+
 <!-- End Jumbotron -->
 
 <!-- Content -->
